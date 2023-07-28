8c21662f336b8d68240b0da908db6991b7373ae3
Raj1128 <118893926+Raj1128@users.noreply.github.com>
2023-07-28 18:27:47 +0530

    Add files via upload

---
+++ ContactHeader.jsx
@@ -0,0 +1,14 @@
+
+
+const ContactHeader = () => {
+  return (
+    <div>
+        <h1>CONTACT US</h1>
+        <P>
+        LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU! WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE, EMAIL, OR SOCIAL MEDIA
+        </P>
+    </div>
+  )
+}
+
+export default ContactHeader;
